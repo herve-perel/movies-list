b20b79977d0af02ea6137cb075f42c2d7706de5a
herve <herveperel@gmail.com>
2023-08-14 14:19:41 +0200

    ajout fixture category

--- src/Controller/MovieController.php
+++ src/Controller/MovieController.php
@@ -6,13 +6,22 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+#[Route('/movie', name: 'movie_')]
 class MovieController extends AbstractController
 {
-    #[Route('/movie/', name: 'movie_index')]
+    #[Route('/', name: 'index')]
     public function index(): Response
     {
         return $this->render('movie/index.html.twig', [
            'website' => 'Wild Series',
+        ]);
+    }
+
+    #[Route('/{id}', methods: ['GET'], requirements: ['id' => '\d+'], name: 'show')]
+    public function show(int $id = 4): Response
+    {
+        return $this->render('movie/show.html.twig', [
+            'id' => $id,
         ]);
     }
 }
