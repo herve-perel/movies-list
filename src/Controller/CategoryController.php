8d4af4faccd78902f33e143d3a546e5f5f711ceb
herve <herveperel@gmail.com>
2023-08-15 14:33:04 +0200

    crud

--- src/Controller/CategoryController.php
+++ src/Controller/CategoryController.php
@@ -6,8 +6,10 @@ use App\Entity\Category;
 use App\Repository\CategoryRepository;
 use App\Repository\MovieRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Form\CategoryType;
 
 #[Route('/category', name: 'category_')]
 class CategoryController extends AbstractController
@@ -19,6 +21,24 @@ class CategoryController extends AbstractController
 
         return $this->render('category/index.html.twig', [
             'categories' => $categories,
+        ]);
+    }
+
+    #[Route('/new', name: 'new')]
+    public function new(Request $request, CategoryRepository $categoryRepository): Response
+
+    {
+        $category = new Category();
+        $form = $this->createForm(CategoryType::class, $category);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted()) {
+            $categoryRepository->save($category, true);
+            return $this->redirectToRoute('category_index');
+        }
+
+        return $this->render('category/new.html.twig', [
+            'form' => $form,
         ]);
     }
 
@@ -34,7 +54,7 @@ class CategoryController extends AbstractController
 
         return $this->render('category/show.html.twig', [
             'category' => $category,
-            'movies' => $movieRepository->findBy(['category' => $category],['id' => 'DESC'], 3, 0),
+            'movies' => $movieRepository->findBy(['category' => $category], ['id' => 'DESC'], 3, 0),
         ]);
     }
 }
