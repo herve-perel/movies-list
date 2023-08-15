9111f6216e414b82c186be0ad3d7ae035b980e7f
herve <herveperel@gmail.com>
2023-08-15 10:54:24 +0200

    faker

--- src/DataFixtures/EpisodeFixtures.php
+++ src/DataFixtures/EpisodeFixtures.php
@@ -6,31 +6,25 @@ use App\Entity\Episode;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Common\DataFixtures\DependentFixtureInterface;
 use Doctrine\Persistence\ObjectManager;
+use Faker\Factory;
 
 class EpisodeFixtures extends Fixture implements DependentFixtureInterface
 {
     public function load(ObjectManager $manager): void
     {
-        $episode = new Episode();
-        $episode->setTitle('Welcome to the Playground');
-        $episode->setNumber(1);
-        $episode->setSynopsis('bien bien cool vraiment');
-        $episode->setSeason($this->getReference('season1_Arcane'));
-        $manager->persist($episode);
+        $faker = Factory::create();
 
-        $episode = new Episode();
-        $episode->setTitle('Welcome to the ');
-        $episode->setNumber(2);
-        $episode->setSynopsis('bien bien cool vraiment');
-        $episode->setSeason($this->getReference('season1_Arcane'));
-        $manager->persist($episode);
+        for ($i = 0; $i < 200; $i++) {
+            $episode = new Episode();
+            $episode->setNumber($faker->numberBetween(1, 10));
+            $episode->setTitle($faker->name());
+            $episode->setSynopsis($faker->paragraphs(3, true));
+            $episode->setSeason($this->getReference('season_' . $i % 20));
 
-        $episode = new Episode();
-        $episode->setTitle('Welcome');
-        $episode->setNumber(3);
-        $episode->setSynopsis('bien bien cool vraiment');
-        $episode->setSeason($this->getReference('season1_Arcane'));
-        $manager->persist($episode);
+            $manager->persist($episode);
+        }
+
+        $manager->flush();
 
         $manager->flush();
     }
